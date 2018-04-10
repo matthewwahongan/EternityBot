11dd9f1f9d1b13b6232f3397dd208491acab9daa
MATTHEWWAHONGAN <imanuel.wahongan@gmail.com>
2018-04-10 23:48:23 +0800

    node_modules

---
+++ index.js
@@ -0,0 +1,23 @@
+const botSettings = require('./settings.json');
+const Discord = require('discord.js');
+
+const bot =  new Discord.Client({disableEveryone: true});
+
+
+bot.on("ready", () => {
+    console.log('I\'m Online\nI\'m Online');
+});
+bot.on("message", message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+
+    let MessageArray = message.content.split(" ");
+    let command = MessageArray[0];
+    let args =  MessageArray.slice(1);
+
+    console.log(MessageArray);
+    console.log(command);
+    console.log(args);
+});
+
+bot.login(botSettings.token);
