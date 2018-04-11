3f7f947e13fd0aa601640af3da58f9f3eb62c1de
MATTHEWWAHONGAN <imanuel.wahongan@gmail.com>
2018-04-11 13:10:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 const botSettings = require('./settings.json');
 const Discord = require('discord.js');
 
-const bot =  new Discord.Client({disableEveryone: true});
+const bot = new Discord.Client({disableEveryone: true});
 
 
-bot.on("ready", () => {
+bot.on("ready", async () => {
     console.log('I\'m Online\nI\'m Online');
+    bot.user.setGame("Eternity Bot | Development");
 });
-bot.on("message", message => {
+bot.on("message", async message => {
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
 
-    let MessageArray = message.content.split(" ");
+    let prefix = botSettings.prefix;
+    let MessageArray = message.content.split("");
     let command = MessageArray[0];
     let args =  MessageArray.slice(1);
 
-    console.log(MessageArray);
-    console.log(command);
-    console.log(args);
-});
-bot.on('message', message => {
-    if (message.content === 'ping') {
-      message.reply('pong');
-    }
+    if(command === "!hallo"){
+      return message.channel.send("Hallo!");
+    };
 });
 
 bot.login(process.env.BOT_TOKEN);
