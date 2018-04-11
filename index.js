89586abaf481210a961b88fe8a9a641c62641f79
MATTHEWWAHONGAN <imanuel.wahongan@gmail.com>
2018-04-11 14:47:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ bot.on("message", async (message) => {
 
     if(command === `${prefix}version`) {
       let embed = new Discord.RichEmbed()
-          .setcolor('RANDOM')
+          .setColor('RANDOM')
           .setAuthor("Version")
           .setDescription(`Bot`);
 
